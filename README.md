4acab64a9941cd86570c47167787fdba00912ba4
Drew Immerman <drewimmerman@gmail.com>
2019-01-17 17:20:11 -0500

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # node-mailgun
 Mailgun wrapper for sending emails from NodeJS
+
+## Prerequisites
+
+- Create mailgun account (https://www.mailgun.com/)
+- Create domain
+- Add DNS records
+
+## Installation
+
+```
+npm i StatelessStudio/node-mailgun
+```
+
+## Setup
+
+```typescript
+import { NodeMailgun } from 'node-mailgun';
+
+const mailer = new NodeMailgun();
+mailer.apiKey = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; // Set your API key
+mailer.domain = 'mail.my-sample-app.com'; // Set the domain you registered earlier
+mailer.fromEmail = 'noreply@my-sample-app.com'; // Set your from email
+mailer.fromTitle = 'My Sample App'; // Set the name you would like to send from
+
+mailer.init();
+
+// Send an email to test@example.com
+mailer
+	.send('test@example.com', 'Hello!', '<h1>hsdf</h1>')
+	.then((result) => console.log('Done', result))
+	.catch((error) => console.error('Error: ', error));
+
+```
+
+### 
+
+```typescript
+...
+router.post('/', (request, response, next) => {
+	mailer.send
+		.send('test@example.com', 'Hello!', '<h1>hsdf</h1>')
+		.then(() => next())
+		.catch((error) => response.sendStatus(500));
+})
+...
+```
---
+++ src/node-mailgun.ts
@@ -0,0 +1,123 @@
+import * as Mailgun from 'mailgun-js';
+
+/**
+ * Create a NodeMailgun mailer.
+ * 	You must create a new NodeMailgun() instance, set the necessary members (below),
+ * 	and then call init().  After initialization, you can then call send() to send an
+ * 	email.
+ * 
+ * 	Necessary Members:
+ * 		- apiKey Mailgun API Key
+ * 		- domain Mailgun registered domain
+ * 		- fromEmail Email address to send from (Does not have to exist)
+ * 		- fromTitle Email address sender name
+ * 
+ * @param apiKey string (Optional) Mailgun API Key
+ * @param domain string (Optional) Mailgun registered domain
+ */
+export class NodeMailgun {
+	// Mailgun core object
+	public mailgun;
+
+	// Mailgun API Key
+	public apiKey: string;
+
+	// Mailgun Domain Name
+	public domain: string;
+
+	// Sender email address
+	public fromEmail: string;
+
+	// From email title
+	public fromTitle: string;
+
+	// String to prepend to message subject
+	public subjectPre?: string = '';
+
+	// String to append to message subject
+	public subjectPost?: string = '';
+
+	// Message body header
+	public header?: string = '';
+
+	// Message body footer
+	public footer?: string = '';
+
+	/**
+	 * Constructor
+	 * @param apiKey (Optional) string Mailgun API Key
+	 * @param domain (Optional) string Mailgun registered domain
+	 */
+	constructor(apiKey?: string, domain?: string) {
+		// Set mailgun config
+		this.apiKey = apiKey;
+		this.domain = domain;
+	}
+
+	/**
+	 * Initialize
+	 */
+	public init() {
+		// Check input
+		if (!this.apiKey) {
+			throw new Error('Please pass a valid API Key to NodeMailgun()');
+		}
+
+		if (!this.domain) {
+			throw new Error('Please pass a valid domain to NodeMailgun()');
+		}
+
+		if (!this.fromEmail) {
+			throw new Error('Please set NodeMailgun::fromEmail');
+		}
+
+		if (!this.fromTitle) {
+			throw new Error('Please set NodeMailgun::fromTitle');
+		}
+
+		// Initialize Mailgun
+		this.mailgun = new Mailgun({
+			apiKey: this.apiKey,
+			domain: this.domain
+		});
+
+		return this;
+	}
+
+	/**
+	 * Send a message
+	 * @param to string Email Address to send message to
+	 * @param subject string Message subject
+	 * @param body string Message body
+	 */
+	public send(to: string, subject: string, body: string) {
+		return new Promise(async (accept, reject) => {
+			// Check mailgun
+			if (!this.mailgun) {
+				reject(
+					'Please call NodeMailgun::init() before sending a message!'
+				);
+			}
+
+			// Create subject
+			subject = this.subjectPre + subject + this.subjectPost;
+
+			// Create body
+			body = this.header + body + this.footer;
+
+			// Create message parts
+			const message = {
+				from: `${this.fromTitle} <${this.fromEmail}>`,
+				to: to,
+				subject: subject,
+				html: body
+			};
+
+			// Send email
+			this.mailgun.messages().send(message, (error, body) => {
+				// Pass result through Promise
+				error ? reject(error) : accept(body);
+			});
+		});
+	}
+}
