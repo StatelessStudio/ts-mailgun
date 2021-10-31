cf4ec7d74b2c818fb89553c0257386b7f8480c84
Drew Immerman <drewimmerman@gmail.com>
2021-10-31 15:07:22 -0400

    Load test config from .env

--- example/environment.ts
+++ example/environment.ts
@@ -5,7 +5,16 @@ const envfile = dotenv.config();
  * Environment Variables Schema
  */
 export class Environment {
-	APP_TITLE = 'MY_APP';
+	APP_TITLE = 'My Sample App';
+
+	MAILGUN_TEST_MODE = true;
+	MAILGUN_API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+	MAILGUN_DOMAIN = 'mail.my-sample-app.com';
+	MAILGUN_FROM_EMAIL = 'noreply@my-sample-app.com';
+	MAILGUN_NEWSLETTER = 'newsletter@my-sample-app.com';
+
+	TEST_EMAIL_TO = 'johndoe@example.com';
+	TEST_EMAIL_IMAGE = 'example/assets/images/jpg.png';
 }
 
 // Export
@@ -13,3 +22,5 @@ export const env: Environment = Object.assign(
 	new Environment(),
 	envfile.parsed
 );
+
+env.MAILGUN_TEST_MODE = envfile.parsed.MAILGUN_TEST_MODE === 'true' ? true : false;
--- example/index.ts
+++ example/index.ts
@@ -1,70 +1,89 @@
 import { NodeMailgun, Attachment } from '../src/';
 import * as fs from 'fs';
 
+import { env } from './environment';
+
 // Setup mailgun
 const mailer = new NodeMailgun();
-mailer.testMode = true;
-mailer.apiKey = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-mailer.domain = 'mail.my-sample-app.com';
-mailer.fromEmail = 'noreply@my-sample-app.com';
-mailer.fromTitle = 'My Sample App';
+mailer.testMode = env.MAILGUN_TEST_MODE;
+mailer.apiKey = env.MAILGUN_API_KEY;
+mailer.domain = env.MAILGUN_DOMAIN;
+mailer.fromEmail = env.MAILGUN_FROM_EMAIL;
+mailer.fromTitle = env.APP_TITLE;
 
 // Initialize mailgun & newsletter mailing list
 mailer.init();
-mailer.initMailingList('newsletter@mail.my-sample-app.com');
+mailer.initMailingList(env.MAILGUN_NEWSLETTER);
 
 // Async function
 async function main() {
-	// Send email to john.doe@example.com
+	// Send email to drewimmerman@gmail.com
 	await mailer
-		.send('john.doe@example.com', 'Hello John', '<h1>John,</h1>')
+		.send(env.TEST_EMAIL_TO, 'Hello John', '<h1>John,</h1>')
 		.catch(console.error);
 
 	// Add user to mailing list
 	await mailer
-		.listAdd('john.doe@example.com', 'John Doe', { id: 1 })
+		.listAdd(env.TEST_EMAIL_TO, 'John Doe', { id: 1 })
 		.catch(console.error);
 
 	// Get user list
-	let users = await mailer.getList().catch(console.error);
+	const users = await mailer.getListAddresses().catch(console.error);
+	console.log('users', users);
 
 	// Send mailing list
 	await mailer
-		.listSend('newsletter@mg.stateless.studio', 'Test Newsletter %recipient_name%', `This newsletter is a test!`)
+		.listSend(env.MAILGUN_NEWSLETTER, 'Test Newsletter %recipient_name%', 'This newsletter is a test!')
 		.catch(console.error);
 
 	// Send file image
 	await mailer.send(
-		'john.doe@example.com',
+		env.TEST_EMAIL_TO,
 		'File Image',
 		'sadf',
 		{},
 		{
-			attachment: '~/Downloads/jpg.png'
+			attachment: env.TEST_EMAIL_IMAGE
 		}
 	);
 
 	// Send buffer image
 	await mailer.send(
-		'john.doe@example.com',
+		env.TEST_EMAIL_TO,
 		'Buffer Image',
 		'sadf',
 		{},
 		{
-			attachment: fs.readFileSync('~/Downloads/jpg.png')
+			attachment: fs.readFileSync(env.TEST_EMAIL_IMAGE)
 		}
-	)
+	);
 
 	// Send stream image
 	await mailer.send(
-		'john.doe@example.com',
+		env.TEST_EMAIL_TO,
 		'Stream Image',
 		'sadf',
 		{},
 		{
-			attachment: fs.createReadStream('~/Downloads/jpg.png')
+			attachment: fs.createReadStream(env.TEST_EMAIL_IMAGE)
+		}
+	);
+
+	// Send attachment object
+	await mailer.send(
+		env.TEST_EMAIL_TO,
+		'Attachment Object',
+		'sadf',
+		{},
+		{
+			attachment: new Attachment({
+				data: fs.createReadStream(env.TEST_EMAIL_IMAGE),
+				filename: 'bob.png'
+			})
 		}
 	);
 }
 
-main();
+main()
+	.then(console.log)
+	.catch(console.error);
